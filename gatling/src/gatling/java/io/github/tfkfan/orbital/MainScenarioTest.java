1d6744f7a65cf5a625415f36d87e0784c77a3c85
tfkfan <sylar131@live.ru>
2026-01-10 22:47:38 +0300

    Added gatling

--- gatling/src/gatling/java/io/github/tfkfan/orbital/MainScenarioTest.java
+++ gatling/src/gatling/java/io/github/tfkfan/orbital/MainScenarioTest.java
@@ -1,5 +1,6 @@
 package io.github.tfkfan.orbital;
 
+import io.gatling.javaapi.core.ChainBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
@@ -7,6 +8,8 @@ import io.github.tfkfan.orbital.core.configuration.MessageTypes;
 import io.github.tfkfan.orbital.core.room.RoomType;
 import io.github.tfkfan.orbital.message.MessageBuilder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.Duration;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
@@ -19,26 +22,31 @@ public class MainScenarioTest extends Simulation {
             http.wsBaseUrl("ws://localhost:8085")
                     .wsUnmatchedInboundMessageBufferSize(100);
 
-    final ScenarioBuilder scenario = scenario(MAIN_SCENARIO).exec(
-            ws("Connect")
-                    .connect("/game")
-                    .onConnected(exec(
-                            ws("Join")
-                                    .sendText(MessageBuilder.joinMessage(RoomType.TRAINING).toString())
-                                    .await(2)
-                                    .on(ws.checkTextMessage("Join:check")
-                                                    .check(jsonPath("$.type").is(Integer.toString(MessageTypes.GAME_ROOM_JOIN_SUCCESS))),
-                                            ws.checkTextMessage("Start:check")
-                                                    .check(jsonPath("$.type").is(Integer.toString(MessageTypes.GAME_ROOM_START))))
-                                    .await(6)
-                                    .on(ws.checkTextMessage("BattleStart:check")
-                                            .check(jsonPath("$.type").is(Integer.toString(MessageTypes.GAME_ROOM_BATTLE_START)))
-                                    )
-                    ))
-    );
+    final ScenarioBuilder scenario = scenario(MAIN_SCENARIO)
+            .exec(ws("Connect").connect("/game"))
+            .exec(ws("Join")
+                    .sendText(MessageBuilder.joinMessage(RoomType.TRAINING).toString())
+                    .await(2)
+                    .on(ws.checkTextMessage("Join:check")
+                                    .check(jsonPath("$.type").ofInt().is(MessageTypes.GAME_ROOM_JOIN_SUCCESS)),
+                            ws.checkTextMessage("Start:check")
+                                    .check(jsonPath("$.type").ofInt().is(MessageTypes.GAME_ROOM_START)))
+                    .await(6)
+                    .on(ws.checkTextMessage("BattleStart:check")
+                            .check(jsonPath("$.type").ofInt().is(MessageTypes.GAME_ROOM_BATTLE_START))
+                    )
+            )
+            .during(Duration.ofSeconds(10)).on(
+                    ws("GameRoom")
+                            .sendText("{}")
+                            .await(Duration.ofMillis(55))
+                            .on(ws.checkTextMessage("RoomUpdate:check")
+                                    .check(jsonPath("$.type").ofInt().in(MessageTypes.UPDATE, MessageTypes.MESSAGE),
+                                            jsonPath("$.data").exists()))
+            );
 
     {
-        setUp(scenario.injectOpen(constantUsersPerSec(2).during(20)))
+        setUp(scenario.injectOpen(atOnceUsers(10)))
                 .protocols(httpProtocol);
     }
 }
